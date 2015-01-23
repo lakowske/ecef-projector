59e730ffa72e49eac23cbf448c4c7fc8dc447544
Seth Lakowske <lakowske@gmail.com>
2015-01-22 19:41:34 -0600

    fixed import on ecef example

---
+++ ecef.js
@@ -0,0 +1,6 @@
+var ECEFProjector = require('./');
+var projector = new ECEFProjector();
+var xyz = projector.project(37.8043722, -122.2708026, 0.0);
+console.log(xyz);
+var gps = projector.unproject(xyz[0], xyz[1], xyz[2]);
+console.log(gps);
